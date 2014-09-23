e2cf9445fac3a51d8b549bbf3e395fabb59a4e76
Jonathan Dong <jonathan.dong@intel.com>
2014-09-23 14:46:15 +0800

    Imported EchoApp and EchoExtension projects.
    EchoExtension is designed to create an extension based on crosswalk's
    extension API, and EchoApp is designed to create a demo application
    to run the echo demo, which will load the EchoExtension as a framework.

---
+++ demos/echo/EchoExtension/EchoExtension/EchoExtension.h
@@ -0,0 +1,19 @@
+//
+//  EchoExtension.h
+//  EchoExtension
+//
+//  Created by Jonathan Dong on 14/9/23.
+//  Copyright (c) 2014年 Crosswalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EchoExtension.
+FOUNDATION_EXPORT double EchoExtensionVersionNumber;
+
+//! Project version string for EchoExtension.
+FOUNDATION_EXPORT const unsigned char EchoExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EchoExtension/PublicHeader.h>
+
+
