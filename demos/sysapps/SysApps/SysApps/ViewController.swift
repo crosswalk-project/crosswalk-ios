9aa7e7048e1a091dd84ada4c0cf95b8c02ff49c4
Jonathan Dong <jonathan.dong@intel.com>
2014-10-12 15:46:59 +0800

    Imported sysapp demo's html5 assets.
    Imported from crosswalk-demos projects:
    https://github.com/crosswalk-project/crosswalk-demos

--- demos/sysapps/SysApps/SysApps/ViewController.swift
+++ demos/sysapps/SysApps/SysApps/ViewController.swift
@@ -3,19 +3,19 @@
 // found in the LICENSE file.
 
 import UIKit
+import CrosswalkiOS
 
-class ViewController: UIViewController {
+class ViewController: XWalkViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        if let path = NSBundle.mainBundle().pathForResource("index", ofType: "html", inDirectory:"app") {
+            loadURL(NSURL.fileURLWithPath(path)!)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
