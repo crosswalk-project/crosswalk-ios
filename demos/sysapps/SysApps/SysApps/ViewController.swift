547d54ce8a5fc9f0070cf57ab7f0707b15f2a9fe
Jonathan Dong <jonathan.dong@intel.com>
2014-10-12 14:43:10 +0800

    Created demo app SysApps
    This app is to import the SysApps demo from crosswalk-demos project,
    which will demostrate the sysapps api of crosswalk.
    Also create the SysApps.xcworkspace and included CrosswalkiOS
    framework.

---
+++ demos/sysapps/SysApps/SysApps/ViewController.swift
@@ -0,0 +1,21 @@
+// Copyright (c) 2014 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
