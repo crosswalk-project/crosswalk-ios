1fa51e68471b8c8d3e53f0cc5b0ea6d18deab0c6
Jonathan Dong <jonathan.dong@intel.com>
2014-10-29 12:31:21 +0800

    Make SysApps build

---
+++ src/sysapps/app/ViewController.swift
@@ -0,0 +1,32 @@
+// Copyright (c) 2014 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import UIKit
+import WebKit
+import CrosswalkLite
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var userContentController = WKUserContentController()
+
+        var config : WKWebViewConfiguration = WKWebViewConfiguration()
+        config.userContentController = userContentController
+        var webview = WKWebView(frame: view.frame, configuration: config, extendable: true)
+        webview.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+        webview.frame = view.frame
+        webview.navigationDelegate = self
+        view.addSubview(webview)
+
+        if let path = NSBundle.mainBundle().pathForResource("index", ofType: "html", inDirectory:"www") {
+            webview.loadRequest(NSURLRequest(URL: NSURL.fileURLWithPath(path)!));
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
