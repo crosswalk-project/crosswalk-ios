189e3c0bfb4e0fc84e5ad28c524aaa4b0b9ac20c
Jonathan Dong <jonathan.dong@intel.com>
2014-11-11 14:02:41 +0800

    Create HexGL demo
    Use CocoaPods to add CocoaHTTPServer into project, and started the
    embedding server when app is loaded.
    
    NOTICE: please follow the instruction on: http://cocoapods.org/
    to install and use cocoapods.

---
+++ src/hexgl/HexGLApp/HexGLApp/HexGL-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  HexGL-Bridging-Header.h
+//  HexGLApp
+//
+//  Created by Jonathan Dong on 11/7/14.
+//  Copyright (c) 2014 Crosswalk. All rights reserved.
+//
+
+#ifndef HexGLApp_HexGL_Bridging_Header_h
+#define HexGLApp_HexGL_Bridging_Header_h
+
+//#include "GCDWebServer.h"
+//#include "GCDWebServerDataResponse.h"
+#include "HTTPServer.h"
+
+#endif
